f3cdafb26041d7dd0a5b1c2bc9d33075859433e0
philo37373 <119889363+philo37373@users.noreply.github.com>
2023-03-16 22:35:32 -0500

    Add files via upload

---
+++ Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,44 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(50)   NOT NULL,
+    "description" varchar(100)   NOT NULL,
+    "goal" int   NOT NULL,
+    "pledged" int   NOT NULL,
+    "outcome" varchar(50)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(50)   NOT NULL,
+    "currency" varchar(50)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(50)   NOT NULL,
+    "subcategory_id" varchar(50)   NOT NULL
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(50)   NOT NULL,
+    "category" varchar(50)   NOT NULL
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(50)   NOT NULL,
+    "sub-category" varchar(50)   NOT NULL
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL
+);
+
+ALTER TABLE "category" ADD CONSTRAINT "pk_category_category_id" PRIMARY KEY ("category_id");
+
+ALTER TABLE "subcategory" ADD CONSTRAINT "pk_subcategory_subcategory_id" PRIMARY KEY ("subcategory_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "pk_campaign_contact_id" PRIMARY KEY ("contact_id");
+
